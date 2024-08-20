e5adf758af7d96e49b4393850d1a6380454e13c5
Thiago Ribeiro <thvinicius1@gmail.com>
2024-08-20 06:48:31 -0300

    feat:inicio do método salvar processador

--- src/main/java/com/inventario/controller/ProcessadorController.java
+++ src/main/java/com/inventario/controller/ProcessadorController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.inventario.models.ProcessadorModel;
 import com.inventario.service.ProcessadorService;
@@ -17,12 +20,25 @@ public class ProcessadorController {
 	@Autowired
 	ProcessadorService processadorService;
 	
+//	@RequestMapping(value="/processadores", method = RequestMethod.GET)
+//	public ModelAndView getProcessadores() {
+//		ModelAndView mv = new ModelAndView("processadores");
+//		List<ProcessadorModel> processadores = processadorService.findAll();
+//		mv.addObject("processadores", processadores);
+//		return mv;
+//	}
+	
 	@RequestMapping(value="/processadores", method = RequestMethod.GET)
-	public ModelAndView getProcessadores() {
-		ModelAndView mv = new ModelAndView("processadores");
-		List<ProcessadorModel> processadores = processadorService.findAll();
-		mv.addObject("processadores", processadores);
-		return mv;
+	public String getPostProcessadores() {
+		return "processadores";
 	}
 	
+	@RequestMapping(value="/processadores", method = RequestMethod.POST)
+	public String salvarProcessador(ProcessadorModel processadorModel, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			return "redirect:/processadores";
+		}
+		processadorService.save(processadorModel);
+		return "redirect:/processadores";
+	}
 }
